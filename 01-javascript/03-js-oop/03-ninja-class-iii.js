2e6abbeaa6a94c5860cbbc5c5b9f879068244919
Njomza <njomzaalaj@gmail.com>
2019-03-16 21:06:57 +0100

    Ninja Class III

---
+++ 01-javascript/03-js-oop/03-ninja-class-iii.js
@@ -0,0 +1,55 @@
+
+//Part I
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log(`My name is ${this.name}`);
+    }
+
+    showStats() {
+        console.log(`Name: $(this.name), Strength: $(this.strength), Speed: $(this.speed), Health: $(this.health)`)
+    }
+
+    drinkSake() {
+
+        this.health += 10;
+        return this;
+    }
+}
+
+//Part II
+
+class Sensei extends Ninja {
+    constructor(name){
+        super(name);
+        this.health=200;
+        this.speed = 10;
+        this.strength=10;
+        this.wisdom=10;
+    }
+
+    speakWisdom() {
+        const message = super.drinkSake();
+        console.log(message);
+    }
+
+    showStats() {
+        console.log(`Name: $(this.name), Strength: $(this.strength), Speed: $(this.speed), Health: $(this.health)`)
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
+
+
+
+
+
+
