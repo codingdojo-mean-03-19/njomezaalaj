33bb28bb9fad5b807b90af7b81c2a0d69d4947b0
Njomeza Alaj <njomzaalaj@gmail.com>
2019-04-02 11:25:47 +0200

    Update index.js

--- 03-express/02-socket.io/02-epic-button-game/routes/index.js
+++ 03-express/02-socket.io/02-epic-button-game/routes/index.js
@@ -5,15 +5,17 @@ module.exports = function Route(app, server){
     app.get('/', function(req,res){
         res.render("index");
     });
+   
+    var counter = 0;
 
     io.sockets.on('connection', function(socket){
         socket.on("update", function(data){
-            io.emit('updated_message', {response: count++});
+            io.emit('updated_message', {response: counter++});
         });
 
 
         socket.on("reset", function(data){
-            io.emit('updated_message', {response: count=0});
+            io.emit('updated_message', {response: counter=0});
 
         });
     });
