f2a973e65f0edae5879886ba5a6e202145f9793f
Njomeza Alaj <njomzaalaj@gmail.com>
2019-05-15 11:18:12 +0200

    anonymous notes

---
+++ 06-full-mean/01-integration/01-anonymous_notes/notes/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const parser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const { PORT: port = 8000} = process.env;
+require('./server/config/database');
+
+app.use(express.static(path.join(__dirname, 'dist/notes')));
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+app.use(require('./server/routes'));
+
+
+
+
+
+app.listen(port, () => console.log(`express listening on port ${ port }`));
